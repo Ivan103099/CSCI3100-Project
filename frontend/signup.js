4e68adcc99a4e3eaeeba75c4c7127b14a9ce8836
Ivan103099 <ivon103099@gmail.com>
2025-04-08 23:13:19 +0800

    Add password checklist

--- frontend/signup.js
+++ frontend/signup.js
@@ -2,6 +2,8 @@ const passwordField = document.getElementById("password");
 const togglePassword = document.querySelector(".password-toggle-icon i");
 const repPasswordField = document.getElementById("rep-password");
 const repTogglePassword = document.querySelector(".rep-password-toggle-icon i");
+const passwordSame = document.getElementById("password-same");
+const passwordLength = document.getElementById("password-length");
 
 function viewPassword() {
     const newType = passwordField.type === "password" ? "text" : "password";
@@ -13,4 +15,19 @@ function viewPassword() {
     });
 }
 
+function validatePassword() {
+    if (passwordField.value === repPasswordField.value) {
+        passwordSame.innerHTML = '<i class="bi bi-check-lg"></i>Passwords match';
+    } else {
+        passwordSame.innerHTML = '<i class="bi bi-x-lg"></i>Passwords do not match';
+    }
 
+    if (passwordField.value.length >= 8 && repPasswordField.value.length >= 8) {
+        passwordLength.innerHTML = '<i class="bi bi-check-lg"></i>At least 8 characters long';
+    } else {
+        passwordLength.innerHTML = '<i class="bi bi-x-lg"></i>At least 8 characters long';
+    }
+}
+
+passwordField.addEventListener("input", validatePassword);
+repPasswordField.addEventListener("input", validatePassword);
