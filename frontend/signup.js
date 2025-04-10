2a152ec0e13ca6676775932690e2cdc0f1f66b8c
Ivan103099 <ivon103099@gmail.com>
2025-04-10 16:17:38 +0800

    Edit password checklist in signup page

--- frontend/signup.js
+++ frontend/signup.js
@@ -4,6 +4,7 @@ const repPasswordField = document.getElementById("rep-password");
 const repTogglePassword = document.querySelector(".rep-password-toggle-icon i");
 const passwordSame = document.getElementById("password-same");
 const passwordLength = document.getElementById("password-length");
+const signupButton = document.getElementById("signup-btn");
 
 function viewPassword() {
     const newType = passwordField.type === "password" ? "text" : "password";
@@ -16,17 +17,23 @@ function viewPassword() {
 }
 
 function validatePassword() {
+    let isPasswordValid = true;
+
     if (passwordField.value === repPasswordField.value) {
         passwordSame.innerHTML = '<i class="bi bi-check-lg"></i>Passwords match';
     } else {
         passwordSame.innerHTML = '<i class="bi bi-x-lg"></i>Passwords do not match';
+        isPasswordValid = false;
     }
 
     if (passwordField.value.length >= 8 && repPasswordField.value.length >= 8) {
         passwordLength.innerHTML = '<i class="bi bi-check-lg"></i>At least 8 characters long';
     } else {
         passwordLength.innerHTML = '<i class="bi bi-x-lg"></i>At least 8 characters long';
+        isPasswordValid = false;
     }
+
+    signupButton.disabled = !isPasswordValid;
 }
 
 passwordField.addEventListener("input", validatePassword);
